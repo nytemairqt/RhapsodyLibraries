4db664bcbce2d64e83461c970ec2c83658188626
Cody <nytemairqt@gmail.com>
2025-03-22 19:16:43 +1000

    Implemented Tethys

---
+++ Tethys/Scripts/ScriptProcessors/Tethys/Interface.js
@@ -0,0 +1,57 @@
+/*
+    Copyright 2023, 2024, 2025 iamlamprey
+
+    This file is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with This file. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+Content.makeFrontInterface(1000, 710);
+
+/* Rhapsody Includes */
+include("RhapsodyBoilerplate/includes/Ui.js");
+include("RhapsodyBoilerplate/includes/LookAndFeel.js");
+include("RhapsodyBoilerplate/includes/Paths.js");
+include("RhapsodyBoilerplate/includes/Expansions.js");
+include("RhapsodyBoilerplate/includes/Header.js");
+include("RhapsodyBoilerplate/includes/Footer.js");
+include("RhapsodyBoilerplate/includes/Presets.js");
+include("RhapsodyBoilerplate/includes/UserSettings.js");
+include("RhapsodyBoilerplate/includes/ZoomHandler.js");
+
+/* NEAT Includes */
+include("NEATBoilerplate/includes/NEATRhapsodyRefs.js");
+include("NEATBoilerplate/includes/NEATRandom.js");
+include("NEATBoilerplate/includes/NEATUi.js");
+include("NEATBoilerplate/includes/NEATChaosEngine.js");
+include("NEATBoilerplate/includes/NEATTooltip.js");
+include("NEATBoilerplate/includes/NEATStoreButton.js");function onNoteOn()
+{
+	
+}
+ function onNoteOff()
+{
+	
+}
+ function onController()
+{
+	
+}
+ function onTimer()
+{
+	
+}
+ function onControl(number, value)
+{
+	
+}
+ 
